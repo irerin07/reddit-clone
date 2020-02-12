903fd730c5d9ad569ff354be0b42e94bf9d5308a
choiwono <treasure_b@naver.com>
2020-02-12 23:39:28 +0900

    backToTop 버튼 추가

---
+++ reddit-frontend/src/components/BackToTops/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBackToTops = styled.button`
+    
+`;
+
+const BackToTops = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BackToTops;
