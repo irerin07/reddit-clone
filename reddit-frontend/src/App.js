56a46f9587a5157080bf54cdf759bfe0fb4ad5e4
choiwono <treasure_b@naver.com>
2020-02-12 22:19:37 +0900

    page, component, 폴더구조 변경

--- reddit-frontend/src/App.js
+++ reddit-frontend/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import PostListPage from './pages/PostListPage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage';
-import UserProfile from './pages/UserProfile';
-import UserSetting from './pages/UserSetting'
-import WritePage from './pages/WritePage'
+import Header from './layouts/PageHeader'
+import Routes from './routes'
 
 const App = () => {
   return (
     <>
-      <Route component={PostListPage} path="/" exact />
-      <Route component={LoginPage} path="/login" />
-      <Route component={RegisterPage} path="/register" />
-      <Route component={UserProfile} path="/user/:username" />
-      <Route component={UserSetting} path="/settings" />
-      <Route component={WritePage} path="/write" />
+      <Header />
+      <Routes />
     </>
   );
 }
